6508ceb4cd6f6a40d1b7dd47c1cce5c3b699254d
Muhammmad Zain <zainchoudery786@gmail.com>
2026-01-18 14:13:32 +0500

    Cinfig added

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -3,5 +3,6 @@ dotenv.config();
 
 export const ENV ={
     PORT:process.env.PORT,
-    DB_URL:process.env.DB_URL
+    DB_URL:process.env.DB_URL,
+    NODE_ENV:process.env.NODE_ENV
 };
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import path from 'path';
 import {ENV} from './lib/env.js';
 
 const app=express();
+
+const __dirname=path.resolve()
 
 
 const PORT=ENV.PORT|| 3000;
@@ -9,5 +12,20 @@ app.listen(3000,()=>{
     console.log(`Server is running on localhost:${PORT}`)
 })
 
+app.get("/help",(req,res)=>{
+    res.json({
+        message:"Help Api Message"
+    })
+})
+
+
+// For Production
+if(ENV.NODE_ENV==="production") {
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+    app.get("/{*any}",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+    })
+}
 
 
